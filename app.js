7fb3cb222f9a1c3ede29a4f037ec1a5cef1d794f
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2021-03-24 11:33:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'block';
+addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -12,7 +12,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
   addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'block';
+    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
