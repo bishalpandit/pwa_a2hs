812ab2bb26cce93d6c8f4eb6864dbe6d824238ac
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2021-03-24 12:37:56 +0530

    Changed display to block

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+addBtn.style.display = 'block';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
